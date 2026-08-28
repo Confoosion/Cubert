e21f0c8bcead0700000a4e0af9a9b9abce30619e
Jake <jakesales@gmail.com>
2026-08-28 16:05:32 -0700

    Added a Main Menu
    No art yet. Just simple buttons. The start button works and goes straight to the game.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject dimBG;
+
+    private float fadeSpeed = 1.5f;
+    private Coroutine startRoutine = null;
+
+    public void StartGame()
+    {
+        if(startRoutine == null)
+        {
+            startRoutine = StartCoroutine(StartGameAnim());
+        }
+        // SceneManager.LoadScene("Game");
+    }
+
+    IEnumerator StartGameAnim()
+    {
+        dimBG.SetActive(true);
+        Image image = dimBG.GetComponent<Image>();
+        Color currentColor = image.color;
+        float currentAlpha = currentColor.a;
+
+        while(currentAlpha < 1f)
+        {
+            currentAlpha += Time.deltaTime * fadeSpeed;
+            currentColor.a = currentAlpha;
+
+            image.color = currentColor;
+            yield return null;
+        }
+
+        startRoutine = null;
+
+        SceneManager.LoadScene("Game");
+        yield return null;
+    }
+}
