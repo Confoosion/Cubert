9eb9c039dba678318add465e4aaca2dc60a983c3
Jake <jakesales@gmail.com>
2026-08-31 12:55:46 -0700

    Added 4 more Cuberts
    Iubert, Rubert, Qubert, and Dubert

--- Assets/Scripts/EyeLook.cs
+++ Assets/Scripts/EyeLook.cs
@@ -7,6 +7,8 @@ public class EyeLook : MonoBehaviour
     [SerializeField] private float xRadius = 0.2f;
     [SerializeField] private float yRadius = 0.2f;
 
+    [SerializeField] private bool inverseEyes = false;
+
     private Camera cam;
 
     private void Awake()
@@ -17,13 +19,16 @@ public class EyeLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(xRadius <= 0f && yRadius <= 0f)
+            return;
+    
         Vector3 mouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPos.z = eyeCenter.position.z;
 
         Vector2 direction = (Vector2)mouseWorldPos - (Vector2)eyeCenter.position;
         
-        Vector2 normalized = new Vector2(direction.x / xRadius, direction.y / yRadius);
-        Vector2 clampedNormalized = Vector2.ClampMagnitude(normalized, 1f);
+        Vector2 normalized = new Vector2((xRadius > 0f) ? direction.x / xRadius : 0f, (yRadius > 0f) ? direction.y / yRadius : 0f);
+        Vector2 clampedNormalized = Vector2.ClampMagnitude(inverseEyes ? -normalized : normalized, 1f);
         Vector2 clampedOffset = new Vector2(clampedNormalized.x * xRadius, clampedNormalized.y * yRadius);
 
         pupil.position = (Vector2)eyeCenter.position + clampedOffset;
